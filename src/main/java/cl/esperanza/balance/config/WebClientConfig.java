43a77d0cae40362298b1a7d7d98a5e7da536a389
CyCklop <ignacioandresflores06@gmail.com>
2026-07-02 11:08:20 -0400

    añadido values en config

--- src/main/java/cl/esperanza/balance/config/WebClientConfig.java
+++ src/main/java/cl/esperanza/balance/config/WebClientConfig.java
@@ -3,21 +3,22 @@ package cl.esperanza.balance.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient telemetriaWebClient(){
-        return WebClient.builder().baseUrl("http://localhost:8086/api/v1/telemetria").build();
+    public WebClient telemetriaWebClient(@Value("${telemetria.service.url:http://localhost:8086/api/v1/telemetria}") String telemetriaServiceUrl){
+        return WebClient.builder().baseUrl(telemetriaServiceUrl).build();
     }
 
     @Bean
-    public WebClient facturacionWebClient(){
-        return WebClient.builder().baseUrl("http://localhost:8083/api/v1/facturacion").build();
+    public WebClient facturacionWebClient(@Value("${facturacion.service.url:http://localhost:8083/api/v1/facturacion}") String facturacionServiceUrl){
+        return WebClient.builder().baseUrl(facturacionServiceUrl).build();
     }
     
     @Bean
-    public WebClient incidenciasWebClient(){
-        return WebClient.builder().baseUrl("http://localhost:8082/api/v1/incidencias").build();
+    public WebClient incidenciasWebClient(@Value("${incidencias.service.url:http://localhost:8082/api/v1/incidencias}") String incidenciasServiceUrl){
+        return WebClient.builder().baseUrl(incidenciasServiceUrl).build();
     }
 }
